2fb89a5f26d18548f1ef402090cdf7225448e846
Murugesan <2505282@cognizant.com>
2026-05-20 03:39:36 -0700

    Added comment in Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Second line added!");
+        // This line added in Lab-5
     }
 }
